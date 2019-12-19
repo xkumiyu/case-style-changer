2148ed4e29fbd2cbb1554adebed8780269b2a0e8
xkumiyu <xkumiyu@gmail.com>
2019-12-20 01:38:21 +0900

    Implement the function to convert from space_separated to camel_case

--- case_style_changer/cli.py
+++ case_style_changer/cli.py
@@ -1,10 +1,45 @@
 import argparse
 
 
-class CSCArgumentParser(argparse.ArgumentParser):
-    pass
+class CaseGuesser(object):
+    def guess(self, text):
+        if self.is_space_separated(text):
+            return 'space_separated'
+        else:
+            raise Exception('Invalid case')
+
+    def is_space_separated(self, text):
+        return ' ' in text
+
+
+def split_text(text, case):
+    """ split text into words
+    """
+
+    if case == 'space_separated':
+        return text.split(' ')
+    else:
+        raise
+
+
+def camel_case(words):
+    """ convert text to camel case
+    """
+    words = [words[0].lower()] + [word.capitalize() for word in words[1:]]
+    return ''.join(words)
+
+
+def change_case(text):
+    guesser = CaseGuesser()
+    case = guesser.guess(text)
+
+    words = split_text(text, case)
+    return camel_case(words)
 
 
 def main():
-    parser = CSCArgumentParser(description='Change case style.')
-    parser.parse_args()
+    parser = argparse.ArgumentParser(description='Change case style.')
+    parser.add_argument('text')
+    args = parser.parse_args()
+
+    print(change_case(args.text))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,31 @@
-from case_style_changer.cli import CSCArgumentParser
+from case_style_changer.cli import CaseGuesser, camel_case, change_case, split_text
 
 
-def test_parser():
-    CSCArgumentParser()
+def test_camel_case():
+    words = ['case', 'style', 'changer']
+    text = camel_case(words)
+
+    assert text == 'caseStyleChanger'
+
+
+def test_split_text():
+    text = 'case style changer'
+    case = 'space_separated'
+    words = split_text(text, case)
+
+    assert words == ['case', 'style', 'changer']
+
+
+def test_guess():
+    text = 'case style changer'
+    guesser = CaseGuesser()
+    case = guesser.guess(text)
+
+    assert case == 'space_separated'
+
+
+def test_change_case():
+    text = 'case style changer'
+    changed_text = change_case(text)
+
+    assert changed_text == 'caseStyleChanger'
