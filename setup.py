00ce157eb4de4d5d0f61fff55bd0dd8c5da8fd9d
xkumiyu <xkumiyu@gmail.com>
2019-12-21 23:09:43 +0900

    docs: update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,30 @@ from setuptools import find_packages, setup
 
 import case_style_changer
 
-setup(name='case-style-changer',
-      version=case_style_changer.__version__,
-      description=case_style_changer.__doc__.strip(),
-      author=case_style_changer.__author__,
-      author_email='xkumiyu@gmail.com',
-      url='https://github.com/xkumiyu/case-changer',
-      license=case_style_changer.__licence__,
-      entry_points={
-          'console_scripts': [
-              'csc = case_style_changer.cli:main',
-              'case_style_changer = case_style_changer.cli:main'
-          ]
-      },
-      packages=find_packages(exclude=('tests', 'docs')),
-      setup_requires=['wheel'])
+setup(
+    name='case-style-changer',
+    version=case_style_changer.__version__,
+    description=case_style_changer.__doc__.strip(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author=case_style_changer.__author__,
+    author_email='xkumiyu@gmail.com',
+    url='https://github.com/xkumiyu/case-style-changer',
+    license=case_style_changer.__licence__,
+    entry_points={
+        'console_scripts': [
+            'csc = case_style_changer.cli:main',
+            'case_style_changer = case_style_changer.cli:main'
+        ]
+    },
+    packages=find_packages(exclude=('tests', 'docs')),
+    setup_requires=['wheel'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
