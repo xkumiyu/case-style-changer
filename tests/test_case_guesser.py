96c3533038ff4a461cfe5712590074fdef7bef22
xkumiyu <xkumiyu@gmail.com>
2019-12-23 00:40:02 +0900

    test: add test case

--- tests/test_case_guesser.py
+++ tests/test_case_guesser.py
@@ -1,16 +1,23 @@
 import pytest
 
 from case_style_changer.case_guesser import CaseGuesser
+from case_style_changer.case_style import HyphenSeparated
 from case_style_changer.case_style import SpaceSeparated
+from case_style_changer.case_style import UnderscoreSeparated
+from case_style_changer.case_style import UppercaseSeparated
 
 
-def test_guess():
-    text = 'case style changer'
-
+@pytest.mark.parametrize('text, expected',
+                         [('case style changer', SpaceSeparated),
+                          ('case_style_changer', UnderscoreSeparated),
+                          ('case-style-changer', HyphenSeparated),
+                          ('caseStyleChanger', UppercaseSeparated),
+                          ('CaseStyleChanger', UppercaseSeparated)])
+def test_guess(text, expected):
     guesser = CaseGuesser()
     case = guesser.guess(text)
 
-    assert case == SpaceSeparated
+    assert case == expected
 
 
 def test_guess_error():
